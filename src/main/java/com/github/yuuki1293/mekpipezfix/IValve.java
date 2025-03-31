1bc52d623e10b8bc87775bc4586ffc6efb441e94
yuuki1293 <yuuki1293@outlook.jp>
2025-03-31 22:36:12 +0900

    feat: reconnect insert fluid

--- src/main/java/com/github/yuuki1293/mekpipezfix/IValve.java
+++ src/main/java/com/github/yuuki1293/mekpipezfix/IValve.java
@@ -1,6 +1,7 @@
 package com.github.yuuki1293.mekpipezfix;
 
 import de.maxhenkel.pipez.blocks.tileentity.PipeLogicTileEntity;
+import de.maxhenkel.pipez.blocks.tileentity.PipeTileEntity;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
@@ -12,11 +13,13 @@ public interface IValve {
      */
     default void mekpipezfix$updatePipezCache(BlockEntity self, Direction[] sides) {
         for (Direction side : sides) {
-            var pipezSide = side.getOpposite();
             var level = self.getLevel();
             if (level == null) return;
-            var be = level.getBlockEntity(self.getBlockPos().relative(side));
+            var pipezSide = side.getOpposite();
+            var pipePos = self.getBlockPos().relative(side);
+            var be = level.getBlockEntity(pipePos);
 
+            PipeTileEntity.markPipesDirty(level, pipePos);
             if (be instanceof PipeLogicTileEntity pipez && pipez.isExtracting(pipezSide)) {
                 pipez.setExtracting(pipezSide, true);
             }
